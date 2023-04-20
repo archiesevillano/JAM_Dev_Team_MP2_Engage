90ccfb72e0b1f5d562906405bb644dd9c07f8dd5
archiesevillano <archie.sevillano29@gmail.com>
2023-04-20 09:06:42 +0800

    #1 - Collaboration Test

---
+++ assets/scripts/index.js
@@ -0,0 +1,9 @@
+import { loadLocalSource, sourceLoad } from "./resources.js";
+
+// const image = document.querySelector("#sample");
+
+// const initSource = async () => {
+//     sourceLoad({ object: image, source: await loadLocalSource("image", "logo.svg") });
+// }
+
+// initSource();
