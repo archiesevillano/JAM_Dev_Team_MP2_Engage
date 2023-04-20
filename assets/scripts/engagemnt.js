9bfd2bd9b6589e622a814df8caef7a702f5f8e89
unknown <osunero.marc@gmail.com>
2023-04-20 11:35:56 +0800

    #1 engagement.js added

---
+++ assets/scripts/engagemnt.js
@@ -0,0 +1,21 @@
+// Objects
+const card = document.getElementsByClassName("engagement-cards");
+const cardName = document.getElementsByClassName("engagement-cards-name");
+const cardTitle = document.getElementsByClassName("engagement-info");
+const cardApply = document.getElementsByClassName("engagement-bottom"); 
+const cardImage = document.createElement("img");
+const cardNamed = document.createElement("p");
+const jobTitle = document.createElement("p");
+const compLogo = document.createElement("img");
+const applicants = document.createElement("p");
+
+
+// append 
+card.appendChild("cardImage");
+cardName.appendChild("cardNamed");
+cardTitle.appendChild("jobTitle");
+cardTitle.appendChild("compLogo");
+cardApply.appendChild("applicants");
+
+
+
