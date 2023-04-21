3e0686700a7977a0899a863b429713f099a592ee
unknown <osunero.marc@gmail.com>
2023-04-22 01:42:18 +0800

    #14 added comments on get age function

--- assets/scripts/form.js
+++ assets/scripts/form.js
@@ -66,13 +66,19 @@ confirmShowPassword.addEventListener("click", () => {
 
 // GET AGE FUNC
 const getAge = (month, day, year) => {
+    // Declare Variable to get present date
     const today = new Date();
+    // Declare variable for formula of present date -1 because january is presented as 0
     const birthdate = new Date(year, month - 1, day);
+    // Declare age with today and your birthdate
     let age = today.getFullYear() - birthdate.getFullYear();
+    // Declare monthDiff to get difference between today and birthdate
     const monthDiff = today.getMonth() - birthdate.getMonth()
+    // if with lofical operators to check if value is true or false
     if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < birthdate.getDate())){
       age--;
     }
+    // return age after function
     return age;
   };
   
