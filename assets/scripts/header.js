4e3478126b5dc730afd68851d6f069021dbb094c
archiesevillano <archie.sevillano29@gmail.com>
2023-04-21 20:28:16 +0800

    #14 notification, messages, menu box added

--- assets/scripts/header.js
+++ assets/scripts/header.js
@@ -1,3 +1,5 @@
+
+//Show/Hide the notification box when profile is clicked
 export const profileDropdown = () => {
     const dropDown = document.querySelector(".profile-drop-down");
 
@@ -10,5 +12,34 @@ export const profileDropdown = () => {
 
 }
 
+export const notificationDropDown = () => {
+    const notificationBox = document.querySelector(".notification-box");
+
+    if (notificationBox.style.display == "flex") {
+        notificationBox.style.display = "none";
+    }
+    else {
+        notificationBox.style.display = "flex";
+    }
+}
+
+export const messageDropDown = () => {
+    const messageBox = document.querySelector(".messages-box");
+
+    if (messageBox.style.display == "flex") {
+        messageBox.style.display = "none";
+    }
+    else {
+        messageBox.style.display = "flex";
+    }
+}
+
 const user_profile = document.querySelector("#user-profile-button");
 user_profile.addEventListener("click", profileDropdown);
+
+const notificationBtn = document.querySelector(".notification-btn");
+notificationBtn.addEventListener("click", notificationDropDown);
+
+
+const messageBtn = document.querySelector(".message-btn");
+messageBtn.addEventListener("click", messageDropDown);
