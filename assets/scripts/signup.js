361beb3605ffed8af6e210bf88a10a4092914432
unknown <osunero.marc@gmail.com>
2023-04-25 15:23:59 +0800

    #24 commit  to pull

--- assets/scripts/signup.js
+++ assets/scripts/signup.js
@@ -5,9 +5,8 @@ const load = () => {
     cancelBtn.addEventListener('click', (e) => {
         e.preventDefault();
         window.history.back();
-        console.log("sefsef");
     });
-
+    // toggle password show
     const togglePs = document.querySelector(".new-password-toggle-btn");
     const toggleConfirmPs = document.querySelector(".confirm-password-toggle-btn");
     const passwordField = document.querySelector(".password");
@@ -21,52 +20,54 @@ const load = () => {
     populateMonths();
     populateYears();
     populateDays();
-
+    // End of show toggle password
+    // Sign up 
     const signIn = document.querySelector(".sign-up-btn");
     signIn.addEventListener('click', e => {
         e.preventDefault();
 
-        const validateForm = (firstName, lastName, email, password, confirmPassword, agreementChecked, dropdownValue, middleName = '') => {
+       
+          const firstName = document.querySelector("#firstname");
+          const lastName = document.querySelector("#lastname");
+          const email = document.querySelector("#email");
+          const password = document.querySelector("#password");
+          const confirmPassword = document.querySelector("#confirm-password");
           const specialChars = /[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
           const uppercaseChars = /[A-Z]/;
           const lowercaseChars = /[a-z]/;
           const emailRegex = /^[A-Za-z0-9]+[\w.-]*[A-Za-z0-9]+@[A-Za-z0-9]+\.[A-Za-z]{2,}$/;
           
-          function validateForm() {
             if (firstName.value.trim() === '' || lastName.value.trim() === '' || email.value.trim() === '' || password.value.trim() === '' || confirmPassword.value.trim() === '' || dropdownValue === '') {
               console.log('Error: All fields are required');
               return false;
             }
-            else if (!emailRegex.test(email.value)) {
+            if (!emailRegex.test(email.value)) {
               console.log('Error: Invalid email format');
               return false;
             }
-            else if (!specialChars.test(password.value) || !uppercaseChars.test(password.value) || !lowercaseChars.test(password.value) || password.value.length < 8 || /\s/.test(password.value)) {
+            if (!specialChars.test(password.value) || !uppercaseChars.test(password.value) || !lowercaseChars.test(password.value) || password.value.length < 8 || /\s/.test(password.value)) {
               console.log('Error: Password must contain at least one special character, one uppercase letter, one lowercase letter, no whitespace and be at least 8 characters long');
               return false;
             }
-            else if (!agreementChecked) {
+            if (!agreementChecked) {
               console.log('Error: You must agree to the terms and conditions');
               return false;
             }
-            else if (password.value !== confirmPassword.value) {
+            if (password.value !== confirmPassword.value) {
               console.log('Error: Passwords do not match');
               return false;
             }
-            else if (dropdownValue === '') {
+            if (dropdownValue === '') {
               console.log('Error: Please select a value from the dropdown');
               return false;
             }
             console.log('Success: Form validated!');
             return true;
-          };
-          
-          
           // Example usage:
-         
-    }
+          }
+          // End of sign up
     
-});
+);
 
 load();
 
