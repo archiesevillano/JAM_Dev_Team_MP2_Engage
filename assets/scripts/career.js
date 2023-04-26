dea83325846eb8bb226fb4c171e93bc13610acd3
unknown <osunero.marc@gmail.com>
2023-04-27 04:33:09 +0800

    #31 Adding containers to create cards and fetch data

--- assets/scripts/career.js
+++ assets/scripts/career.js
@@ -25,29 +25,65 @@ const headernavburger = () => {
       modal.style.display = "none";
     }
   }
-
-const dropdownBtn = document.querySelector('.drop-btn');
-const dropdownContent = document.querySelector('.dropdown-content');
-
-const selectCategory = () => {
-  dropdownContent.addEventListener('click', event => {
-    if (event.target.tagName === 'A') {
-      const selectedCategory = event.target.textContent;
-      dropdownBtn.textContent = selectedCategory;
-    }
-  });
-}
-
-selectCategory();
-
-async function getData() {
-  try {
+  
+  
+  const dropdownBtn = document.querySelector('.drop-btn');
+  const dropdownContent = document.querySelector('.dropdown-content');
+  const cardsContainer = document.querySelector('.cards-container');
+  
+  // Fetch data from JSON file
+  const fetchData = async () => {
     const response = await fetch(`./../assets/db/data.json`);
     const data = await response.json();
-    console.log(data);
-  } catch (error) {
-    console.error(error);
+    return data;
   }
-}
-
-getData();
+  
+  containerCards.className = ".card-container";
+  jobsPostedCards.className = ".jobposted-cards";
+  jobImgContainer.className = ".job-image";
+  jobImg.className = ".job-image";
+  jobTextContent.className = ".jobtext-content";
+  jobName.className = ".job-name";
+  jobNameText.className = ".job-name-text";
+  companyHiring.className = ".company-hiring";
+  viewJobBtn.className = ".viewjob-btn";
+  openModal.className = ".open-modal";
+  modal.className = ".modal";
+  modalContent.className = ".modal-content";
+  closer.className = ".close";
+  jobDescriptContain.className = ".job-description-container";
+  jobDescriptionH3.className = ".job-description";
+  jobDescpBtn.className = ".jobdescp-btn";
+  applyJobBtn.className = ".jobdescp-btn";
+  
+  
+  const containerCards = document.createElement('div');
+  const jobsPostedCards = document.createElement('div');
+  const jobImgContainer = document.createElement('div');
+  const jobImg = document.createElement('img');
+  const jobTextContent = document.createElement('div');
+  const jobName = document.createElement('div');
+  const jobNameText = document.createElement('p');
+  const companyHiring = document.createElement('p');
+  const viewJobBtn = document.createElement('div');
+  const openModal = document.createElement('button');
+  const modalDiv = document.createElement('div');
+  const modalContent = document.createElement('div');
+  const closer = document.createElement('span');
+  const jobDescriptContain = document.createElement('div');
+  const jobDescriptionH3 = document.createElement('h3');
+  const jobDescrpt = document.createElement('p');
+  const jobDescpBtn = document.createElement('div');
+  const applyJobBtn = document.createElement('button');
+  
+  containerCards.appendChild(jobsPostedCards);
+  jobsPostedCards.appendChild(jobImgContainer, jobTextContent, viewJobBtn, modal, jobDescriptContain, modalContent);
+  jobImgContainer.appendChild(jobImg);
+  jobDescriptContain.appendChild(recName, timePost);
+  jobDescriptContain.appendChild(jobDescriptionH3, jobDescrpt);
+  jobDescpBtn.appendChild(applyJobBtn);
+  engInfo.appendChild(engInfoNum);
+  engBot.appendChild(engBotBtn, engBotBtn2);
+  
+  
+  
